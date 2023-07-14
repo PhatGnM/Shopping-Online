eaa4d440c73ae4e166ee71dd862bd0a12a73176e
PhatGnM <139516946+PhatGnM@users.noreply.github.com>
2023-07-14 08:09:58 +0700

    add files
    add project

---
+++ ShoppingOnline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.frwm7qw.mongodb.net',
+    DB_USER: 'phatgnm',
+    DB_PASS: '1234567890',
+    DB_DATABASE: 'shoppingonlone',
+    EMAIL_USER: 'phatgnm@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'uqrllbpfffahpyri',
+    JWT_SECRET: '123',
+    JWT_EXPIRES: '6000000', // in milliseconds
+  };
+  module.exports = MyConstants;
